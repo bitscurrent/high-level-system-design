3029105ee4d5b6d19ffa7e35050c73d026748507
bitscurrent <dilipboiya.office@gmail.com>
2024-08-24 09:56:57 +0530

    remove garbage

--- server/uploadServices/controllers/kafkapublisher.controller.js
+++ server/uploadServices/controllers/kafkapublisher.controller.js
@@ -1,55 +1,3 @@
-// // import KafkaConfig from "../kafka/kafka.js";
-// import KafkaConfig from "../kafka/kafka.js"
-
-// const sendMessageToKafka = async (req, res) => {
-//    console.log("got here in upload service...")
-//    try {
-//        const message = req.body
-//        console.log("body : ", message)
-//        const kafkaconfig = new KafkaConfig()
-//        const msgs = [
-//            {
-//                key: "key1",
-//                value: JSON.stringify(message)
-//            }
-//        ]
-//        const result = await kafkaconfig.produce("transcode", msgs)
-//        console.log("result of produce : ", result)
-//        res.status(200).json("message uploaded successfully")
-
-//    } catch (error) {
-//        console.log(error)
-//    }
-// }
- 
-
-// const pushVideoForEncodingToKafka = async(title, url) => {
-//     try {
-//         const message = {
-//             "title": title,
-//             "url": url
-//         }
-//         console.log("body : ", message)
-//         const kafkaconfig = new KafkaConfig()
-//         const msgs = [
-//             {
-//                 key: "video",
-//                 value: JSON.stringify(message)
-//             }
-//         ]
-//         const result = await kafkaconfig.produce("transcode", msgs)
-//         console.log("result of produce : ", result)
-//         res.status(200).json("message uploaded successfully")
- 
-//     } catch (error) {
-//         console.log(error)
-//     }
-// }
-
-
-// export {pushVideoForEncodingToKafka, sendMessageToKafka}
-
-
 import KafkaConfig from "../kafka/kafka.js";
 
 const sendMessageToKafka = async (req, res) => {
@@ -94,3 +42,5 @@ const pushVideoForEncodingToKafka = async (title, url) => {
 };
 
 export { pushVideoForEncodingToKafka, sendMessageToKafka };
+
+
